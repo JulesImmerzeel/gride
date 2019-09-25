20a5a96b08264e70a15688e437f0305c659bd7c0
avie0108 <45458508+avie0108@users.noreply.github.com>
2019-09-25 15:09:41 +0200

    Added Keys Added required Added validation

--- Models/EmployeeModel.cs
+++ Models/EmployeeModel.cs
@@ -2,25 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Gride.Models
 {
 	public class EmployeeModel
 	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public uint EmployeeID { get; set; }
+		[Required]
+		[StringLength(50)]
 		public string Name{ get; set; }
+		[Required]
+		[StringLength(50)]
 		public string LastName { get; set; }
 		public DateTime DoB { get; set; }
-		public Gender Gender { get; set; }
+		public Gender Gender { get; set; } = Gender.Not_Specified;
+		[Required]
+		[StringLength(100)]
+		[EmailAddress]
 		public string EMail { get; set; }
+		[Required]
+		[StringLength(12)]
+		[Phone]
 		public string PhoneNumber { get; set; }
-		public bool Admin { get; set; }
+		public bool Admin { get; set; } = false;
 		public ulong Skills { get; set; }
 		public int Function { get; set; }
 		public ulong LoginID { get; set; }
 		public float Experience { get; set; }
 		public uint Locations { get; set; }
-		public string ProfileImage { get; set; }
+		[RegularExpression(@"(\\\\?([^\\/]*[\\/])*)([^\\/]+)$", ErrorMessage = "Path to ProfileImage is not a valid path")]
+		public string ProfileImage { get; set; } = null;
 	}
 
 	public enum Gender
