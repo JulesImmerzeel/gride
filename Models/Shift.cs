a0cbf20aa9635b4c48a8dfc8cad38915b1b1b2bb
avie0108 <45458508+avie0108@users.noreply.github.com>
2019-10-02 12:09:25 +0200

    Shift Aanpassingen

--- Models/Shift.cs
+++ Models/Shift.cs
@@ -9,19 +9,16 @@ namespace Gride.Models
 {
 	public class Shift
 	{
-		[Key]
-		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-		public ulong ShiftID { get; set; }
+		public ulong ID { get; set; }
 		public DateTime Start { get; set; }
 		public DateTime End { get; set; }
 		[Key]
 		[ForeignKey("Location")]
 		public uint LocationID { get; set; }
-		[Required]
-		public List<int> Skills { get; set; } = new List<int>();
 		[Key]
 		[ForeignKey("Function")]
 		public int FunctionID { get; set; }
 		public byte MaxEmployees { get; set; }
+		public virtual ICollection<Skill> Skills { get; set; }
 	}
 }
