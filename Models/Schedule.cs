0f1d44de010206546fe799c4c491c98aaf2ffdfa
JulesImmerzeel <45227130+JulesImmerzeel@users.noreply.github.com>
2019-10-11 11:31:23 +0200

    partialview

---
+++ Models/Schedule.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gride.Models
+{
+    public class Schedule
+    {
+        public DateTime today = DateTime.Today;
+        
+    }
+}
