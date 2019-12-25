5320f8af5aa3005849ee921ebabc2cda4445b0d0
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2019-12-25 21:26:39 +0100

    Model created for feedback

---
+++ Models/Feedback.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gride.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string FeedbackDescription { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime FeedbackPostDate { get; set; }
+    }
+}
