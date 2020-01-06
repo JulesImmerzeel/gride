956656b6c3818654fc2321f7dd8612f70ada3b1a
Hicham010 <47334105+Hicham010@users.noreply.github.com>
2020-01-06 12:48:48 +0100

    Feedback view create

--- Controllers/FeedbackController.cs
+++ Controllers/FeedbackController.cs
@@ -12,11 +12,38 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Gride.Controllers
 {
+    [Authorize]
     public class FeedbackController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly SignInManager<IdentityUser> signInManager;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public FeedbackController(ApplicationDbContext context,
+                                SignInManager<IdentityUser> signInManager,
+                                UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+            _context = context;
+            this.signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Create()
+        {
+            if (signInManager.IsSignedIn(User) && _context.EmployeeModel.Single(x => x.EMail == User.Identity.Name).Admin)
+            {
+                return View();
+
+            }
+            else
+            {
+                return Forbid();
+            }
         }
     }
 }
